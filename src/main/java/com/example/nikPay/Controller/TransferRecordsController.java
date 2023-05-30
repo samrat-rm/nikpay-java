ff771f5f7cf974182bfbe8e03df16866bc962e95
samrat r m <samratrm2@gmail.com>
2023-05-30 16:59:33 +0530

    auth added for transaction history

--- src/main/java/com/example/nikPay/Controller/TransferRecordsController.java
+++ src/main/java/com/example/nikPay/Controller/TransferRecordsController.java
@@ -4,14 +4,12 @@ import com.example.nikPay.Model.TransferRecords;
 import com.example.nikPay.Service.TransferRecordService;
 import com.example.nikPay.Service.UserService;
 import com.example.nikPay.TransactionType;
-
+import com.example.nikPay.Config.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -19,15 +17,21 @@ import java.util.List;
 public class TransferRecordsController {
     @Autowired
     private TransferRecordService transferRecordService;
-
+    @Autowired
+    private JwtUtil jwtUtil;
     @Autowired
     private UserService userService;
 
     @GetMapping("/transactions/sender/{userId}")
     public ResponseEntity<List<TransferRecords>> getSenderTransactions(
             @PathVariable String userId,
-            @RequestParam TransactionType transactionType
+            @RequestParam TransactionType transactionType,
+            @RequestHeader("token") String token
     ) {
+
+        if (!jwtUtil.verifyToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         if (!userService.isValidUserId(userId)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
